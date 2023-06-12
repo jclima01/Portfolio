41a49d8d66c4e64410e39810baad6978b6ec7630
unknown <juan.mdq22@gmail.com]>
2023-06-12 17:37:24 -0300

    working on portfolio

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,26 +5,28 @@ const About = () => {
     <div name="about" className="w-full h-screen bg-[#0a192f] text-gray-300">
       <div className="flex flex-col justify-center items-center w-full h-full">
         <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
-          <div className="sm:text-right pb-8 pl-4">
+          <div className="md:text-right pb-8 pl-4">
             <p className="text-4xl font-bold inline border-b-4 border-pink-600">
-              About
+              About me
             </p>
           </div>
-          <div></div>
         </div>
-        <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
-          <div className="sm:text-right text-4xl font-bold">
-            <p>
-              Hi. I'm Sai Kumar, nice to meet you. Please take a look around.
-            </p>
+        <div className="max-w-[1000px] w-full grid md:grid-cols-2 gap-8 px-4">
+          <div className="md:text-right text-4xl font-bold">
+            <p>Te invito a que conozcas un poco más sobre mí.</p>
           </div>
           <div>
             <p>
-              I am passionate about building excellent software that improves
-              the lives of those around me. I specialize in creating software
-              for clients ranging from individuals and small-businesses all the
-              way to large enterprise corporations. What would you do if you had
-              a software expert available at your fingertips?
+              Me considero una persona apasionada por el aprendizaje, adquirir
+              conocimientos e interactuar con las personas. Me he formado a lo
+              largo de los años muchas veces de forma autodidacta, así como
+              también en el ambito formal, obteniendo el título de Profesor de
+              matemática para el nivel secundario de la República Argentina,
+              cursando 2 años para técnico superior en análisis en sistemas y
+              obteniendo diferentes certificaciones de programación tanto en
+              CoderHouse como en Henry. Por mi curiosidad, disfruto enfrentando
+              nuevos desafíos y explorando diferentes enfoques para resolver
+              problemas complejos, conformando sólidos equipos de trabajo.
             </p>
           </div>
         </div>
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -71,25 +71,21 @@ const Nav = () => {
           </Link>
         </li>
         <li className="py-6 text-4xl">
-          {" "}
           <Link onClick={handleClick} to="about" smooth={true} duration={500}>
             About
           </Link>
         </li>
         <li className="py-6 text-4xl">
-          {" "}
           <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
             Skills
           </Link>
         </li>
         <li className="py-6 text-4xl">
-          {" "}
           <Link onClick={handleClick} to="work" smooth={true} duration={500}>
             Work
           </Link>
         </li>
         <li className="py-6 text-4xl">
-          {" "}
           <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
             Contact
           </Link>
@@ -97,35 +93,35 @@ const Nav = () => {
       </ul>
 
       {/* Social icons */}
-      <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
+      <div className="hidden lg:flex fixed flex-col top-[35%] right-0">
         <ul>
-          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
+          <li className="w-[160px] h-[60px] flex justify-between items-center mr-[-100px] hover:ml-[-100px] duration-300 bg-blue-600">
             <a
-              className="flex justify-between items-center w-full text-gray-300"
-              href="/"
+              className="flex flex-row-reverse justify-between items-center w-full text-gray-300"
+              href="https://www.linkedin.com/in/jclimacaramico/"
             >
               Linkedin <FaLinkedin size={30} />
             </a>
           </li>
-          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]">
+          <li className="w-[160px] h-[60px] flex justify-between items-center mr-[-100px] hover:ml-[-100px] duration-300 bg-[#333333]">
             <a
-              className="flex justify-between items-center w-full text-gray-300"
-              href="/"
+              className="flex flex-row-reverse justify-between items-center w-full text-gray-300"
+              href="https://github.com/jclima01"
             >
               Github <FaGithub size={30} />
             </a>
           </li>
-          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]">
+          <li className="w-[160px] h-[60px] flex justify-between items-center mr-[-100px] hover:ml-[-100px] duration-300 bg-[#6fc2b0]">
             <a
-              className="flex justify-between items-center w-full text-gray-300"
+              className="flex flex-row-reverse justify-between items-center w-full text-gray-300"
               href="/"
             >
               Email <HiOutlineMail size={30} />
             </a>
           </li>
-          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
+          <li className="w-[160px] h-[60px] flex justify-between items-center mr-[-100px] hover:ml-[-100px] duration-300 bg-[#565f69]">
             <a
-              className="flex justify-between items-center w-full text-gray-300"
+              className="flex flex-row-reverse justify-between items-center w-full text-gray-300"
               href="/"
             >
               Resume <BsFillPersonLinesFill size={30} />
